f6f73e290e4c06d6fcce0b6ef3802c5dcefe50be
Kamil Sieklucki <kamil.sieklucki@neuca.pl>
2019-09-29 23:09:35 +0200

    new

--- data_table_dplyr.R
+++ data_table_dplyr.R
@@ -1,3 +1,5 @@
+#### https://atrebas.github.io/post/2019-03-03-datatable-dplyr/ ####
+
 library(dplyr)
 library(data.table)
 
@@ -78,4 +80,35 @@ dt[, .(cyl_2 = cyl ^ 2)]
 df %>% mutate(cyl = replace(cyl, cyl == 4, 0L))
 
 dt[cyl == 4, cyl := 0L]
+
+# group by ----
+df %>% group_by(cyl, gear) %>% summarise(suma = sum(disp))
+
+dt[, .(suma = sum(disp)), keyby = .(cyl, gear)] # lub by -> The sole difference between by and keyby is that keyby orders the results and creates a key that will allow faster subsetting
+
+# count ----
+df %>% count(cyl)
+
+dt[, .N, keyby = cyl]
+
+# add_count ----
+df %>% add_count(cyl)
+
+dt[, n := .N, by = cyl]
+
+# To further manipulate columns, dplyr includes nine functions: the _all, _at, and _if versions of summarise(), mutate(), and transmute().
+# With data.table, we use .SD, which is a data.table containing the Subset of Data for each group, excluding the column(s) used in by. So, DT[, .SD] is DT itself and in the expression DT[, .SD, by = V4], .SD contains all the DT columns (except V4) for each values in V4 (see DT[, print(.SD), by = V4]). .SDcols allows to select the columns included in .SD.
+# summarise_all ----
+df %>% summarise_all(max)
+
+dt[, lapply(.SD, max)]
+
+# summarise_at ----
+df %>% group_by(am) %>% summarise_at(vars(cyl, gear), list(min, max))
+
+dt[, c(lapply(.SD, min), lapply(.SD, max)), .SDcols = c("cyl", "gear"), keyby = am]
+
+
+
+
 
